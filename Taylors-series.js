d390386fa019d6d96bf6b7388a9a6df54adbfd0e
Himanshu Upadhyay <heman6760@gmail.com>
2020-10-23 23:39:29 +0530

    added Taylors-series

---
+++ Taylors-series.js
@@ -0,0 +1,15 @@
+function Taylor(x, n) {
+  let p = 1;
+  let f = 1;
+  function _Taylor(x, n) {
+    if (n === 0) return 1;
+    let r = _Taylor(x, n - 1);
+    p *= x;
+    f *= n;
+    return (r += p / f);
+  }
+  return _Taylor(x, n);
+}
+
+const taylor = Taylor(4, 50);
+console.log(taylor);
